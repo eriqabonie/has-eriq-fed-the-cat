01fe9f8866a19be7ab7f01b2e865be1416771640
eriq <e.abonie@gmail.com>
2021-12-18 23:25:04 +0000

    app working, issue parsing int

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 
+const {Cat} = require('./models');
+
 const app = express();
 
 app.use(express.json());
 
+
 app.post('/cats', (req, res)=> {
     const body = req.body;
-    res.status(201).send(body);
+    Cat.create(req.body).then(cat => res.status(201).json(cat))
+    //res.status(201).send(body);
 });
 
+app.get('/cats', (req, res)=> {
+    Cat.findAll(req.body).then(cat => res.status(201).json(cat))
+});
+
+//can't seem to parse an integer
+
+app.get('/cats/', (req, res)=> {
+    
+   const CatId = parseInt(req.params.id);
+
+    Cat.findByPk(CatId).then(cat => res.status(201).json(cat))
+});
 
 module.exports = app;
