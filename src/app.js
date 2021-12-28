1b0a512e3298b8e63561620cb20da83772bad9b6
eriq <e.abonie@gmail.com>
2021-12-28 18:07:47 +0000

    edited the start script

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 app.post('/cats', (req, res)=> {
