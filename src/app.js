022cb2298072551a9930d97b775b48d214eef2d3
eriq <e.abonie@gmail.com>
2021-12-28 17:28:51 +0000

    cat app complete

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { json } = require('express/lib/response');
 
 const {Cat} = require('./models');
 
@@ -10,20 +11,36 @@ app.use(express.json());
 app.post('/cats', (req, res)=> {
     const body = req.body;
     Cat.create(req.body).then(cat => res.status(201).json(cat))
-    //res.status(201).send(body);
 });
 
 app.get('/cats', (req, res)=> {
     Cat.findAll(req.body).then(cat => res.status(201).json(cat))
 });
 
-//can't seem to parse an integer
 
-app.get('/cats/', (req, res)=> {
-    
-   const CatId = parseInt(req.params.id);
-
-    Cat.findByPk(CatId).then(cat => res.status(201).json(cat))
+app.get('/cats/:id', (req, res)=> {
+   const catId = req.params.id;
+   Cat.findByPk(catId).then(cat => res.status(201).json(cat))
 });
+
+
+//this returns cats based on a query string // note: you can't have two gets running at once
+
+// app.get('/cats', (req, res)=> {
+
+//    Cat.findAll({where: req.query}).then(cat => res.status(201).json(cat))
+// });
+
+
+app.patch('/cats/:catId', (req, res)=> {
+    Cat.update(req.body, { where: { id: req.params.catId } }).then(cat => res.status(201).json(cat))
+
+});
+
+app.delete('/cats/:catId', (req, res)=> {
+    Cat.destroy({ where: { id: req.params.catId } }).then(cat => res.status(201).json(cat))
+
+});
+
 
 module.exports = app;
